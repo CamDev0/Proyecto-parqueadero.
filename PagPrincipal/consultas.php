52aa9e529da96933b9613b3dfe2c9a246f0655a2
Camilo Barrios <camilobarriosdev@gmail.com>
2023-04-11 21:50:40 -0500

    login terminado

---
+++ PagPrincipal/consultas.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultas | CCAJ</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="icon" href="../img/icon.png">
+
+    <link rel="stylesheet" href="../styles.css">
+
+</head>
+
+<body>
+    <!--Header - nav-->
+
+    <header class="header no-margin no-padding"> 
+        <div class="container">
+            <div class="navbar">
+                <a class="logo" href="#">
+                    <h1 class="logo__name no-margin text-center">Parqueadero<spa class="logo__bold">CCAJ</spa> </h1>
+                </a>
+                    <nav class="nav">
+                        <a href="consultas.php" class="nav__link">Consultas</a>
+                        <a href="crud.php" class="nav__link">Editar Datos</a>
+                        <a href="../index.html" class="nav__link">Salir</a>
+                    </nav>
+            </div>
+        </div>
+    </header>
+    
+    
+    <div class="img-text">
+        <div class="img-banner">
+            <br><br><br><br><br><br>
+            <div class="text__img">
+                <h2 class="no-margin">Página oficial Parqueadero CCAJ</h2>
+                <p class="no-margin">Consulta nuestros servicios</p>
+            </div>
+        </div>
+    </div>
+    
+    <!--Principal content-->
+
+    <section class="container principal-content">
+        
+        <main class="blog">
+            <h3>Página principal</h3>
+
+            <article class="entrada">
+                <div class="entrada__img">
+                    <img src="../img/Principal.jpg" alt="threeImage">
+                </div>
+                
+                <div class="entrada__text">
+                    <h4 class="no-margin">Consultas</h4>
+                    <p>
+                        Aquí puedes consultar los usuarios registrados actualmente en la página :)
+                    </p>
+                </div>
+
+            </article>
+        </main>
+
+    </section>
+
+    <footer class="footer-principal">
+        <div class="container">
+            <div class="navbar"> 
+                <a class="logo" href="#">
+                    <h1 class="logo__name no-margin text-center">Parqueadero<span class="logo__bold">CCAJ</span> </h1>
+                </a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
